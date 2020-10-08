4d86ffe7dbe45c98de9d223a8ad924b1a1f09c16
lavanya <rangaswamy.lavanya@gmail.com>
2020-10-08 23:28:17 +0000

    Muskmelon-part 1

--- harvest.py
+++ harvest.py
@@ -9,28 +9,50 @@ class MelonType(object):
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
         """Initialize a melon."""
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
         self.pairings = []
 
+        print(self.name)
         # Fill in the rest
+        
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
+    
+        self.pairings.append(pairing)
+        print(self.pairings)
+        return self.pairings
+
+
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
         # Fill in the rest
-
+        self.code = new_code
+        print(self.code) #for checking
+        return self.code
+        
 
 def make_melon_types():
     """Returns a list of current melon types."""
-
+    
     all_melon_types = []
+    # new_dict = {}
+    
+    Muskmelon = MelonType('musk','1998','green','seedless','Bestseller','Muskmelon')
 
-    # Fill in the rest
+    Muskmelon.add_pairing("mint")
+    
+    Muskmelon.update_code("musk001")
 
     return all_melon_types
 
@@ -41,6 +63,11 @@ def print_pairing_info(melon_types):
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
+
+
+if __name__ == "__main__":
+    make_melon_types()   
+
 
     # Fill in the rest
 
